1edb345908241aa76fb67a9b8146bcd2a329c366
Gabriella Szebeni-Molnár <gabriella.szebeni@gmail.com>
2024-10-06 13:10:48 +0200

    add demo exercises

--- 0_demo/playground.js
+++ 0_demo/playground.js
@@ -1,1 +1,10 @@
 // set time out, async JS
+
+console.log('G');
+
+setTimeout(() => {
+    console.log('hi')
+}, 2000
+);
+
+console.log('E');
--- 1_demo/playground.js
+++ 1_demo/playground.js
@@ -1,3 +1,25 @@
 import {sleep, openSchrodringerBox, fail, getRandomNumber} from "../promises.js";
 
 // Promise basics
+
+const promise = sleep(2000);
+promise.then(() => {
+    console.log('hi')
+});
+
+promise.then(() => {
+    console.log('hi')
+});
+
+fail()
+    .then(() => {
+        console.log('hi');
+    })
+    .catch(() => {
+        console.log('oops')
+    })
+
+    openSchrodringerBox()
+        .then(() => {
+            console.log('HI');
+        }, () => {console.log('oops')})
